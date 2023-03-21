b00007cf738a867051134cc954ae7a8835566be8
JayGee <jayguarino@gmail.com>
2023-03-20 20:28:47 -0700

    import/export es6

---
+++ node-es6/app2.js
@@ -0,0 +1,20 @@
+import rect from './rectangle2.js';
+
+function solveRect(l, w) {
+    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR:', err.message)
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`)
+            console.log(`Perimeter of rectangle with dimentsions ${l}, ${w} is: ${rectangle.perimeter()}`)
+        }
+    });
+    console.log('This statement is being logged after the call to rect() \n');
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(5, -3);
